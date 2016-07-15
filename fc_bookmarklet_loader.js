567c0995456ea359a3cb6f097b45e2077141beef
Nikolai Alexander-Himlan <Nikolai.AlexanderHimlan@gmail.com>
2016-07-15 14:44:19 -0400

    updated TODO
    updated cookie script url

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -3,7 +3,7 @@ javascript:(
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
     js.setAttribute('id', 'frozenCookieScript');
-    js.setAttribute('src', 'http://rawgit.com/NikolaiAlexanderHimlan/FrozenCookies_TestingGround/test-pages/frozen_cookies.js');
+    js.setAttribute('src', 'http://NikolaiAlexanderHimlan.github.io/NikolaiAlexanderHimlan/FrozenCookies_TestingGround/test-pages/frozen_cookies.js');
     document.head.appendChild(js);
  }()
 );
